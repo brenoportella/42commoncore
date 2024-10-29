72e479bb7db03570dc684120fc9342ef37f697c8
brenoportella <bp.bohry@gmail.com>
2024-10-29 17:15:16 +0000

    ft_memcpy.c

---
+++ libft/libc/part2/ft_memcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bportell <bportell@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/29 15:57:30 by bportell          #+#    #+#             */
+/*   Updated: 2024/10/29 17:14:59 by bportell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t			i;
+	unsigned char	*c_dest;
+	unsigned char	*c_src;
+
+	c_dest = (unsigned char *)dest;
+	c_src = (unsigned char *)src;
+	i = 0;
+	while (i < n)
+	{
+		c_dest[i] = c_src[i];
+		i++;
+	}
+	return (dest);
+}
