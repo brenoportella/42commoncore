f7549afb33bae5347e99d9a66a0c0b3b79afc039
brenoportella <bp.bohry@gmail.com>
2024-11-04 15:17:09 +0000

    need be reviewd by rduro-pe

--- libft/libc/part2/ft_memmove.c
+++ libft/libc/part2/ft_memmove.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: bportell <bportell@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/29 17:34:11 by bportell          #+#    #+#             */
-/*   Updated: 2024/10/31 17:51:03 by bportell         ###   ########.fr       */
+/*   Created: 2024/11/04 15:01:21 by bportell          #+#    #+#             */
+/*   Updated: 2024/11/04 15:16:32 by bportell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,28 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t			i;
-	unsigned char	*n_dest;
-	unsigned char	*n_src;
+	int i;
+	unsigned char *p_dest;
+	unsigned char *p_src;
 
-	n_src = (unsigned char *)src;
-	n_dest = (unsigned char *)dest;
-	i = 0;
-	/* checking if the pointers are null */
-	if (!dest || !src)
-		return NULL;
-	if (n_dest > n_src)
-		while (n-- > 0)
-			n_dest[n] = n_src[n];
-	else
+	p_dest = (unsigned char *)dest;
+	p_src = (unsigned char *)src;
+	if (p_dest < p_src && (p_dest || p_src))
 	{
-		while (i++ < n)
-			n_dest[i] = n_src[i];
+		i = 0;
+		while (i < (int)n)
+		{
+			p_dest[i] = p_src[i];
+			i++;
+		}
+	}
+	else if (p_dest > p_src && (p_dest || p_src))
+	{
+		while ((int)n > 0)
+		{
+			p_dest[n - 1] = p_src[n - 1];
+			n--;
+		}
 	}
 	return (dest);
 }
@@ -41,7 +46,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 int	main(void)
 {
 	char a[] = "Breno Portella";  // src
-	char b[] = "ruidoruid"; // dest
+	char b[] = "ABCDEFGHIJKLMNO"; // dest
 
 	printf("BEFORE FT_MEMMOVE:\nThe src array is: %s\nThe dest array is: %s\n",
 		a, b);
