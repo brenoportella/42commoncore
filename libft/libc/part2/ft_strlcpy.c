751c5cb69e71040cda8162a0b46364a66c5a8ee2
brenoportella <bp.bohry@gmail.com>
2024-11-04 17:00:03 +0000

    add strlcpy.c, but need be reviewed, ii've some doubts

--- libft/libc/part2/ft_strlcpy.c
+++ libft/libc/part2/ft_strlcpy.c
@@ -6,7 +6,53 @@
 /*   By: bportell <bportell@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 17:34:23 by bportell          #+#    #+#             */
-/*   Updated: 2024/10/29 17:34:23 by bportell         ###   ########.fr       */
+/*   Updated: 2024/11/04 16:59:25 by bportell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+size_t	ft_strlen(const char *str);
+
+/*size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}*/
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	src_len;
+	size_t	i;
+
+	src_len = ft_strlen(src);
+    i = 0;
+	if (size > 0)
+	{
+		while (src[i] && i < size - 1)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+	}
+	return (src_len);
+}
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char a[] = "Breno Portella";  // src
+	char b[] = "ABCDEFGHIJKLMNO"; // dest
+
+	printf("BEFORE FT_MEMMOVE:\nThe src array is: %s\nThe dest array is: %s\n",
+		a, b);
+	ft_strlcpy(b, a, 13);
+	printf("AFTER FT_MEMMOVE\nThe src array is: %s\nThe dest array is: %s\n", a,
+		b);
+}
+*/
