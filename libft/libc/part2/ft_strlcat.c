e341abcbdfd0fa5ed6477d895a80607818643ed7
brenoportella <bp.bohry@gmail.com>
2024-11-04 17:38:35 +0000

    add ft_strlcat.c, but i need study it more

--- libft/libc/part2/ft_strlcat.c
+++ libft/libc/part2/ft_strlcat.c
@@ -6,7 +6,39 @@
 /*   By: bportell <bportell@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 17:34:19 by bportell          #+#    #+#             */
-/*   Updated: 2024/10/29 17:34:20 by bportell         ###   ########.fr       */
+/*   Updated: 2024/11/04 17:37:51 by bportell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+size_t	ft_strlen(const char *str);
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	src_len;
+	size_t	dest_len;
+	size_t	i;
+
+	src_len = ft_strlen(src);
+	dest_len = ft_strlen(dest);
+	if (size < dest_len)
+		return (destsize + src_len);
+	while (src[i] && (dest_len + i) < size - 1)
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = 0;
+	return (dest_len + src_len);
+}
