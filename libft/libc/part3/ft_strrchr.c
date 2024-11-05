3c44e6a54f4291bbe3b903c4caada88d89c2d198
brenoportella <bp.bohry@gmail.com>
2024-11-05 16:36:22 +0000

    add ft_strrchr.c

--- libft/libc/part3/ft_strrchr.c
+++ libft/libc/part3/ft_strrchr.c
@@ -6,7 +6,31 @@
 /*   By: bportell <bportell@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 17:34:31 by bportell          #+#    #+#             */
-/*   Updated: 2024/10/29 17:34:32 by bportell         ###   ########.fr       */
+/*   Updated: 2024/11/05 16:36:03 by bportell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*ft_strrchr(const char *s, int c);
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int		i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	while (i > 0)
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i--;
+	}
+    return (0);
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+	printf("The address is: %s", ft_strrchr("brenoportella", 'r'));
+}
+*/
