ad92162e9e72649b4054ff0c31ed0910d43a3cd1
brenoportella <bp.bohry@gmail.com>
2024-11-05 17:38:43 +0000

    add ft_memchr.c

--- libft/libc/part4/ft_memchr.c
+++ libft/libc/part4/ft_memchr.c
@@ -6,7 +6,26 @@
 /*   By: bportell <bportell@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 17:34:42 by bportell          #+#    #+#             */
-/*   Updated: 2024/10/29 17:34:43 by bportell         ###   ########.fr       */
+/*   Updated: 2024/11/05 17:38:13 by bportell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
+
+void	*ft_memchr(const void *s, int c, size_t n);
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*str;
+
+	str = (unsigned char *)s;
+	i = 0;
+	while (str[i] || i < n)
+	{
+		if (str[i] == (char)c)
+			return ((void *)&str[i]);
+		i++;
+	}
+	return (0);
+}
