29a9d616d7b966662d587421c7d62f2a961a8a07
brenoportella <bp.bohry@gmail.com>
2024-11-05 12:27:30 +0000

    add main, index, and comment auxiliar func

--- libft/libc/part2/ft_strlcat.c
+++ libft/libc/part2/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: bportell <bportell@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 17:34:19 by bportell          #+#    #+#             */
-/*   Updated: 2024/11/04 17:37:51 by bportell         ###   ########.fr       */
+/*   Updated: 2024/11/05 12:25:56 by bportell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 size_t	ft_strlen(const char *str);
 
+/*
 size_t	ft_strlen(const char *str)
 {
 	size_t	i;
@@ -24,6 +25,7 @@ size_t	ft_strlen(const char *str)
 		i++;
 	return (i);
 }
+*/
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
 	size_t	src_len;
@@ -33,7 +35,8 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 	src_len = ft_strlen(src);
 	dest_len = ft_strlen(dest);
 	if (size < dest_len)
-		return (destsize + src_len);
+		return (size + src_len);
+	i = 0;
 	while (src[i] && (dest_len + i) < size - 1)
 	{
 		dest[dest_len + i] = src[i];
@@ -42,3 +45,17 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 	dest[dest_len + i] = 0;
 	return (dest_len + src_len);
 }
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char a[] = "Breno Portella";  // src
+	char b[50] = "ABCDEFG"; // dest
+	printf("BEFORE FT_MEMMOVE:\nThe src array is: %s\nThe dest array is: %s\n",
+		a, b);
+	ft_strlcat(b, a, 50);
+	printf("AFTER FT_MEMMOVE\nThe src array is: %s\nThe dest array is: %s\n", a,
+		b);
+}
+*/
