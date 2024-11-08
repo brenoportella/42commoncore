235a679c44e9796f5abbe086371cf00ea73e45d7
brenoportella <bp.bohry@gmail.com>
2024-11-08 15:12:54 +0000

    add ft_atoi.c

--- libft/libc/part4/ft_atoi.c
+++ libft/libc/part4/ft_atoi.c
@@ -6,7 +6,42 @@
 /*   By: bportell <bportell@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 17:34:39 by bportell          #+#    #+#             */
-/*   Updated: 2024/10/29 17:34:40 by bportell         ###   ########.fr       */
+/*   Updated: 2024/11/08 15:11:41 by bportell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_atoi(const char *nptr);
+
+int	ft_atoi(const char *nptr)
+{
+	int	i;
+	int	signal;
+	int	result;
+
+	i = 0;
+	signal = 1;
+	result = 0;
+	while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == ' ')
+		i++;
+	if (nptr[i] == '+' || nptr[i] == '-')
+	{
+		if (nptr[i++] == '-')
+			signal = -signal;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		result = result * 10 + (nptr[i] - 48);
+		i++;
+	}
+	return (result * signal);
+}
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main(void)
+{
+	printf("FT: %d\n", ft_atoi("  42abc54"));
+	printf("C: %d\n", atoi("  42abc54"));
+}
+*/
