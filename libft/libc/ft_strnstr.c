ffc97efc99a2f05714e3381facc133350309f6cd
brenoportella <bp.bohry@gmail.com>
2024-11-11 11:28:52 +0000

    fix the null syntaxe on if condition

--- libft/libc/ft_strnstr.c
+++ libft/libc/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: bportell <bportell@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 17:34:53 by bportell          #+#    #+#             */
-/*   Updated: 2024/11/11 09:48:34 by bportell         ###   ########.fr       */
+/*   Updated: 2024/11/11 10:38:37 by bportell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ char	*ft_strnstr(const char *src, const char *tofind, size_t len);
 char	*ft_strnstr(const char *src, const char *tofind, size_t len)
 {
 	char	*str;
-
 	size_t i;     /*iteração para cada ponto inicial*/
 	size_t start; /*ponto inicial da iteração*/
-	if (tofind[0] == 0)
-		return (0);
+	
 	start = 0;
 	str = (char *)src;
+	if (tofind[0] == '\0')
+		return (str);
 	while (str[start] && start < len)
 	{
 		i = 0;
