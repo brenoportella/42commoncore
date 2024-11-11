f75138ebcb3464a5c3d4907aebe8321d635ac7ba
brenoportella <bp.bohry@gmail.com>
2024-11-11 11:22:42 +0000

    remove proto, change while condition and add casting c

--- libft/libc/ft_memchr.c
+++ libft/libc/ft_memchr.c
@@ -6,13 +6,11 @@
 /*   By: bportell <bportell@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 17:34:42 by bportell          #+#    #+#             */
-/*   Updated: 2024/11/11 09:48:09 by bportell         ###   ########.fr       */
+/*   Updated: 2024/11/11 10:46:28 by bportell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-void	*ft_memchr(const void *s, int c, size_t n);
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
@@ -21,9 +19,9 @@ void	*ft_memchr(const void *s, int c, size_t n)
 
 	str = (unsigned char *)s;
 	i = 0;
-	while (str[i] || i < n)
+	while (i < n)
 	{
-		if (str[i] == (char)c)
+		if (str[i] == (unsigned char)c)
 			return ((void *)&str[i]);
 		i++;
 	}
