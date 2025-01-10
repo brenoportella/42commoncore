fea7e864afbed28c454bb33b90dac5ab92efda64
brenoportella <bp.bohry@gmail.com>
2025-01-10 16:34:12 +0000

    first try gnl

--- [04] get_next_line/test_get_next_line.c
+++ [04] get_next_line/test_get_next_line.c
@@ -6,7 +6,39 @@
 /*   By: bportell <bportell@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 15:51:29 by bportell          #+#    #+#             */
-/*   Updated: 2025/01/09 15:51:30 by bportell         ###   ########.fr       */
+/*   Updated: 2025/01/10 16:33:42 by bportell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 42
+
+int	main(void)
+{
+	char	*buffer;
+	int		my_file;
+	ssize_t bytes_read;
+
+	buffer = malloc(BUFFER_SIZE * sizeof(char));
+	if (!buffer)
+		return (-1);
+	my_file = open("ascii-art.txt", O_RDONLY);
+	if (my_file == -1)
+		return (-1);
+	if (bytes_read == -1)
+		return (-1);
+	while((bytes_read = read(my_file, buffer, BUFFER_SIZE)) > 0)
+	{
+		// write(1, "linha->\n", 9);
+		write(1, buffer, bytes_read);
+	}
+	
+	close(my_file);
+	free(buffer);
+	return 0;
+}
