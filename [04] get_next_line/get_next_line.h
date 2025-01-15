de7b73b7ef4b81ea9b90784d7b75b498738c52e5
brenoportella <bp.bohry@gmail.com>
2025-01-15 09:58:11 +0000

    i'll re-do all those shit

--- [04] get_next_line/get_next_line.h
+++ [04] get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: bportell <bportell@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 15:51:32 by bportell          #+#    #+#             */
-/*   Updated: 2025/01/14 19:02:21 by bportell         ###   ########.fr       */
+/*   Updated: 2025/01/15 09:57:06 by bportell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,5 @@
 # include <sys/stat.h>
 # include <sys/types.h>
 # include <unistd.h>
-
-char	*ft_strchr(const char *s, int c);
-char	*ft_strdup(const char *s);
-char	*ft_strjoin(char const *s1, char const *s2);
-size_t	ft_strlcpy(char *dest, const char *src, size_t size);
-size_t	ft_strlen(const char *str);
-void	*ft_memmove(void *dest, const void *src, size_t n);
-void	*ft_memset(void *s, int c, size_t n);
 
 #endif
--- [04] get_next_line/test_get_next_line.c
+++ [04] get_next_line/test_get_next_line.c
@@ -6,53 +6,11 @@
 /*   By: bportell <bportell@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 15:51:29 by bportell          #+#    #+#             */
-/*   Updated: 2025/01/14 18:56:34 by bportell         ###   ########.fr       */
+/*   Updated: 2025/01/15 09:56:59 by bportell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-//"lorem ipsum iasfiasasfasfijadj -> A"
-char	*get_next_line(int fd)
-{
-	static char	buffer[BUFFER_SIZE];
-	ssize_t		r_val;
-	char		*line;
-	int			i;
-	char		*nl_flag;
-
-	line = NULL;
-	while ((r_val = read(fd, buffer, BUFFER_SIZE)) > 0)
-	{
-		buffer[r_val] = '\0';
-		// devemos ver se a linha está completa dentro do buffer (\n).
-		nl_flag = ft_strchr(buffer, '\n');
-		if (nl_flag != NULL) // se ele encontrou um \n
-		{
-			i = nl_flag - buffer + 1;
-			// criar uma função paralela pra isso
-			line = ft_strjoin(line, buffer);
-			if (line == NULL)
-				return (NULL);
-			// ft_strlcpy(line, buffer, i + 1);
-			// funçao acaba aqui, retorna a line
-			// movendo o conteudo dps do \n para o inicio do buffer,sobrescrevendo o conteudo.
-			// explicar esse conteudo
-			ft_memmove(buffer, nl_flag + 1, BUFFER_SIZE - i);
-			buffer[BUFFER_SIZE - i] = '\0';
-			return (line);
-		}
-		else
-		{
-			line = ft_strjoin(line, buffer);
-			if (line == NULL)
-				return NULL;
-		}
-	}
-	if (r_val == 0 && line == NULL)
-		return (line);
-	return (NULL);
-}
 #include <stdio.h>
 
 int	main(void)
