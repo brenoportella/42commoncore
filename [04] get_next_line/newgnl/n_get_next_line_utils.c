ad8aee9f4a5b7297c503c2e514a138edf89e17b4
brenoportella <bp.bohry@gmail.com>
2025-01-22 17:50:00 +0000

    add a free and a fix logic on _strjoin to respect the content the buffer until the first \n

--- [04] get_next_line/newgnl/n_get_next_line_utils.c
+++ [04] get_next_line/newgnl/n_get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: bportell <bportell@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 13:24:16 by bportell          #+#    #+#             */
-/*   Updated: 2025/01/21 18:00:59 by bportell         ###   ########.fr       */
+/*   Updated: 2025/01/22 17:48:55 by bportell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,9 @@ int	_strlen(char *str)
 }
 char	*_strjoin(char *str1, char *str2)
 {
-	char *new_str;
-	int i;
-	int j;
+	char	*new_str;
+	int		i;
+	int		j;
 
 	if (!str1)
 		str1 = "";
@@ -39,22 +39,49 @@ char	*_strjoin(char *str1, char *str2)
 	i = -1;
 	while (str1[++i])
 		new_str[i] = str1[i];
-	j = -1;
-	while (str2[++j])
-		new_str[i + j] = str2[j];
-	new_str[i + j] = '\0';
+	j = 0;
+	if (str2[j])
+		new_str[i++] = str2[j++];
+	while (str2[j] && str2[j - 1] != '\n')
+		new_str[i++] = str2[j++];
+	new_str[i] = '\0';
+	if (*str1)
+		free(str1);
 	return (new_str);
 }
-char	*_find_caracter(char *str, int caracter)
+int	_find_caracter(char *buffer)
 {
 	int	i;
 
+	if (!buffer)
+		return (0);
 	i = 0;
-	while (str[i])
+	while (buffer[i])
 	{
-		if (str[i] == (char)caracter)
-			return (&str[i]);
+		if (buffer[i] == '\n')
+			return (1);
 		i++;
 	}
 	return (0);
+}
+void	_memalign(char *buffer)
+{
+	int	i;
+	int	j;
+
+	if (!buffer)
+		return ;
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (buffer[i] == '\n')
+	{
+		j = 0;
+		i++;
+		while (buffer[i] && i < BUFFER_SIZE)
+			buffer[j++] = buffer[i++];
+	}
+	while (j < BUFFER_SIZE)
+		buffer[j++] = '\0';
+	return ;
 }
