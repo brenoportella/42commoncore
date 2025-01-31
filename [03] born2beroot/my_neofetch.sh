878c0699816cd2dc0772e0f663479beb51e56ef4
brenoportella <bp.bohry@gmail.com>
2025-01-31 17:57:18 +0000

    add commands and wall, all that is missing LVM one

--- [03] born2beroot/my_neofetch.sh
+++ [03] born2beroot/my_neofetch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# MY OS INFO
+# MY ARCHITETURE
 os=$(uname -a)
 
 # MY CPU INFO (PHYSICAL)
@@ -9,22 +9,42 @@ cpuf=$(grep "physical id" /proc/cpuinfo | wc -l)
 # MY CPU INFO VIRTUAL
 cpuv=$(grep "processor" /proc/cpuinfo | wc -l)
 
-# MY MEMORY RAM
-mem=$(free --mega | awk '$1 == "Mem:" {printf("%i/%iMB (%.2f%%)\n", $3, $2, $3/$2*100)}')
+# MY MEMORY RAM USAGE
+mem=$(free --mega | awk '$1 == "Mem:" {printf("%i/%iMB (%.2f%%)", $3, $2, $3/$2*100)}')
 
 # MY DISK USAGE
-disk=$(df -m | grep "/dev" | grep -v "/boot" | awk '{memory_use += $3} {total += $2} END {printf("%d/%dGb (%d%%)\n",memory_use,total/1024, memory_use/total*100)}')
+disk=$(df -m | grep "/dev" | grep -v "/boot" | awk '{memory_use += $3} {total += $2} END {printf("%d/%dGb (%d%%)",memory_use,total/1024, memory_use/total*100)}')
 
+#MY CPU LOAD
+cpul=$(top -bn1 | grep "Cpu(s)" | awk '{printf "%.1f", $2 + $4}')
 
-# MY NEOFETCH HEHEHE
-my_os= $(grep 'PRETTY_NAME=' /etc/os-release | cut -d'"' -f2)
-my_host=
-my_kernel=
-my_uptime=
-my_packages=
-my_shell=
-my_resolution=
-my_terminal=
-my_cpu=
-my_gpu=
-my_memory=
+#LAST BOOT TIME
+lboot=$(who -b | grep "system" | awk '{printf $3 " " $4}')
+
+#NUMBER TCPS CONNECTIONS
+tcps=$(ss -ta | grep "ESTAB" | wc -l)
+
+#USERS LOGGED
+user=$(users | wc -w)
+
+#MY IP AND MAC ADDRESS
+netw=$echo "IP $(hostname -i) $(ip link | grep "link/ether" | awk '{printf($2)}')"
+
+#SUDO COMMANDS NUMBER
+sudo=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
+
+#THE OUTPUT
+wall "	
+	#Architecture: $os
+	#CPU physical : $cpuf
+	#vCPU : $cpuv
+	#Memory usage: $mem
+	#Disk usage: $disk
+	#CPU load: $cpul
+	#Last boot: $lboot
+	#LVM use: 
+	#Connections TCP : $tcps
+	#User log: $users
+	#Network: $netw
+	#Sudo : $sudo
+"
