3105bc502a56c76f69f495b2105ca667ccd3e38c
breno portella <bp.bohry@gmail.com>
2025-05-16 15:05:14 +0100

    ft_strlen.c check

---
+++ [99] Exams/rank 2/level 1/ft_strlen.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bportell <bportell@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/16 15:03:36 by bportell          #+#    #+#             */
+/*   Updated: 2025/05/16 15:04:29 by bportell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Assignment name  : ft_strlen
+Expected files   : ft_strlen.c
+Allowed functions:
+--------------------------------------------------------------------------------
+
+Write a function that returns the length of a string.
+
+Your function must be declared as follows:
+
+int	ft_strlen(char *str);
+*/
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
