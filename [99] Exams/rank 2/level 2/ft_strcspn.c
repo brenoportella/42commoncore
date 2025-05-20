6255300176af2ee214fc6bcfe9666543f51ef8c3
breno portella <bp.bohry@gmail.com>
2025-05-20 18:20:07 +0100

    ft_strcspn.c check

---
+++ [99] Exams/rank 2/level 2/ft_strcspn.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcspn.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bportell <bportell@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/20 15:10:28 by bportell          #+#    #+#             */
+/*   Updated: 2025/05/20 18:18:40 by bportell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Assignment name	: ft_strcspn
+Expected files	: ft_strcspn.c
+Allowed functions: None
+---------------------------------------------------------------
+
+Reproduce exactly the behavior of the function strcspn
+(man strcspn).
+
+The function should be prototyped as follows:
+
+size_t	ft_strcspn(const char *s, const char *reject);
+*/
+#include <stddef.h>
+
+size_t	ft_strcspn(const char *s, const char *reject)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (s[i])
+	{
+		j = 0;
+		while (reject[j])
+		{
+			if (s[i] == reject[j])
+				return (i);
+			j++;
+		}
+		i++;
+	}
+	return (i);
+}
+#include <stdio.h>
+#include <string.h>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 3)
+	{
+		printf("my version:%li\n", ft_strcspn(argv[1], argv[2]));
+		printf("og version:%li\n", strcspn(argv[1], argv[2]));
+	}
+	return (0);
+}
