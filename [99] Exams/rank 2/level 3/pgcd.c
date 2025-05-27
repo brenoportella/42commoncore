8f3cf5e1e98efc5e88c2d3c1c49a68ee64ba58ca
breno portella <bp.bohry@gmail.com>
2025-05-27 14:27:41 +0100

    pgcd.c check euclides

---
+++ [99] Exams/rank 2/level 3/pgcd.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pgcd.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bportell <bportell@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/27 14:10:17 by bportell          #+#    #+#             */
+/*   Updated: 2025/05/27 14:26:38 by bportell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Assignment name  : pgcd
+Expected files   : pgcd.c
+Allowed functions: printf, atoi, malloc, free
+--------------------------------------------------------------------------------
+
+Write a program that takes two strings representing two strictly positive
+integers that fit in an int.
+
+Display their highest common denominator followed by a newline (It's always a
+strictly positive integer).
+
+If the number of parameters is not 2, display a newline.
+
+Examples:
+
+$> ./pgcd 42 10 | cat -e
+2$
+$> ./pgcd 42 12 | cat -e
+6$
+$> ./pgcd 14 77 | cat -e
+7$
+$> ./pgcd 17 3 | cat -e
+1$
+$> ./pgcd | cat -e
+$
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int	com_div(int a, int b) //	42, 10
+{
+	int swap;
+
+	while (b > 0)
+	{
+		swap = a % b;
+		a = b;
+		b = swap;
+	}
+	return (a);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 3)
+		printf("%d", com_div(atoi(argv[1]), atoi(argv[2])));
+	printf("\n");
+	return (0);
+}
