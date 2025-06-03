0976d69815fc3dd3e085df4a9441dc560feaf195
breno portella <bp.bohry@gmail.com>
2025-06-03 13:27:21 +0100

    func param_number working

--- [05] pipex/srcs/pipex_parsing.c
+++ [05] pipex/srcs/pipex_parsing.c
@@ -1,25 +1,69 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int	count_tokens(char const *str, char c)
+// função para retornar char **cmd (array de strings com o conteudo do cmd)
+
+//check
+// função para contar a quantidade de termos dentro do cmd original para dps alocar o array de strings
+
+
+// função para contar o tamanho de cada parte do cmd (cada palavra)
+
+
+// função para alocar uma string dentro do array
+
+/**
+ * Count the number of parameters in a command string.
+ *
+ * This function analyzes the command string and returns the
+ * number of parameters it contains. It considers text enclosed
+ * in a single (' ') or double (" ") quotes as a single paremeter.
+ * Even it contains spaces.
+ *
+ * @param cmd Command String to analyze.
+ *
+ * @return The number of parameters in the string.
+ *         Returns 0 if the string is empty or NULL;
+ */
+static int	param_number(const char *cmd)
 {
-	int	count;
+	int		count;
+	char	quote;
 
+	if (!cmd)
+		return (0);
 	count = 0;
-	while (*str)
+	while (*cmd)
 	{
-        
+		while (*cmd == ' ' && *cmd)
+			cmd++;
+		if (*cmd)
+			count++;
+		if (*cmd == '\'' || *cmd == '\"')
+		{
+			quote = *cmd++;
+			while (*cmd != quote && *cmd)
+				cmd++;
+			if (*cmd == quote)
+				cmd++;
+		}
+		while (*cmd != ' ' && *cmd)
+			cmd++;
 	}
 	return (count);
 }
 
+// static int param_len(const char *str)
+// {
+
+// }
+
+
 int	main(int argc, char **argv)
 {
-	if (argc != 2)
-	{
-		perror("ERROR: tem que ter 2 argumentos");
-		return (1);
-	}
-	printf("%i quantidade de termos\n", count_tokens(argv[1], ' '));
-	return (0);
+	int num;
+	(void)argc;
+	num = param_number(argv[1]);
+	printf("%i\n", num);
+	if (argv[1])
+		printf("%s\n", argv[1]);
 }
